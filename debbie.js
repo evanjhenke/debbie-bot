187bf033d85b0c6a7b2e95a62d261c8660a7016e
Evan Henke <evanjhenke@gmail.com>
2020-05-24 16:56:07 -0500

    refactoring

--- debbie.js
+++ debbie.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const importEventHandlers = require('./import-event-handlers');
-const token = process.env.token ? process.env.token : require("./config").token;
+const token = process.env.token ? process.env.token : require('./config').token;
 
 importEventHandlers(new Discord.Client());
 client.login(token);
